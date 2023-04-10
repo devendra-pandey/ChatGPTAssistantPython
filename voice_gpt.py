610db9d5aaeab56ab61781d78488ec5ab2ac8874
devsdp <devendra.pandey.mit@gmail.com>
2023-04-10 15:13:48 +0530

    voice gpt  questions

---
+++ voice_gpt.py
@@ -0,0 +1,81 @@
+import openai
+import pyttsx3
+import speech_recognition as sr
+import time
+# import gTTs
+
+openai.api_key = "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"
+
+engine = pyttsx3.init()
+
+def transcribe_audio_to_text(filename):
+    recogniser = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recogniser.record(source)
+    try:
+        return recogniser.recognize_google(audio)
+    except:
+        print('skipping unknown error')
+
+
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens = 4000,
+        n=1,
+        stop=None,
+        temperature = 0.5,
+    )
+    return response["choices"][0]["text"]
+
+def speak_text(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def main():
+    while True:
+        print("Say 'Genius' to start the recording the question..")
+        with sr.Microphone() as source:
+            recognizer = sr.Recognizer()
+            audio = recognizer.listen(source)
+            try:
+                transcription = recognizer.recognize_google(audio)
+                if transcription.lower() == "genius":
+                    filename = "input.wav"
+                    print("Say your question")
+                    with sr.Microphone() as source:
+                        recognizer = sr.Recognizer()
+                        source.pause_threshold = 1
+                        audio = recognizer.listen(source , phrase_time_limit=None , timeout=None)
+                        with open(filename,"wb") as f:
+                            f.write(audio.get_wav_data())
+
+
+                    text = transcribe_audio_to_text(filename)
+                    if text:
+                        print(f"you said: {text}")
+
+                        response = generate_response(text)
+                        print(f"GPT3 Say: {response}")
+
+                        tts = gTTS(text=response , lang='en')
+
+                        tts.save("sample.mp3")
+
+                        speak_text(response)
+
+            except Exception as e:
+                print("An Error ocured: {}".format(e))
+            
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
